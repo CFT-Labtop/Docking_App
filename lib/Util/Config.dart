62ad3e61634acbf78a0ea2bb3f0e4ed6756c4514
Cheng Wai Lim <s1113618@s.eduhk.hk>
2021-09-28 17:39:39 +0800

    1.1.0

--- lib/Util/Config.dart
+++ lib/Util/Config.dart
@@ -1,5 +1,5 @@
 class Config {
   // static String baseURL = "http://hksvr095.sunhinggroup.com:8019/";
-  static String baseURL = "https://dkmsapi-dev.sunhinggroup.com/"; //dev
-  // static String baseURL = "https://dkmsapi-prod.sunhinggroup.com/";  
+  // static String baseURL = "https://dkmsapi-dev.sunhinggroup.com/"; //dev
+  static String baseURL = "https://dkmsapi-prod.sunhinggroup.com/";  
 }
